df1dcf2c69de22c6d782a8654511b82b888e9b28
AishwaryaChvn <85743360+AishwaryaChvn@users.noreply.github.com>
2022-02-06 13:07:16 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+st.header('Welcome to Food Delivery Service')
+nav = st.sidebar.radio("MENU",["Pizza-100/-","Burger-150/-","Sandwich-100/-","Cutlet-150/-","Idli-80/-","Dosa-100/-"])
+st.image('image.jpg')
+
 components.html(
     """
     <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
